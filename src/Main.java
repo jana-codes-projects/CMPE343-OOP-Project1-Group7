d186855bc597af8e8aa7f9f7cc94b111293a45a9
jana <janabarazi@stu.khas.edu.tr>
2025-10-18 01:02:10 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void WelcomeScreen()
+    {
+        System.out.println("******************************************");
+        System.out.println("WELCOME TO GROUP 7's FIRST PROJECT FOR OOP");
+        System.out.println("Our group members are as follows:");
+        System.out.println("1) Canan Albarazi");
+        System.out.println("2) Emir Furkan Gökdemir");
+        System.out.println("3) Nur Kambasar");
+        System.out.println("4) Kaan Yılmaz");
+        System.out.println("******************************************\n");
+    }
+
+    public static void PrintMainMenu()
+    {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~ MAIN MENU ~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("[A] Primary School\n" +
+                "[B] Secondary School\n" +
+                "[C] High School\n" +
+                "[D] University\n" +
+                "[E] Terminate");
+        System.out.print("Please select a letter corresponding with your submenu choice: ");
+    }
+
+    public static boolean ValidSubmenu(char c)
+    {
+        if (c == 'a' || c == 'b' || c == 'c' || c == 'd' || c == 'e')
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args)
+    {
+        WelcomeScreen();
+
+        char letter = 'j'; // dummy value
+        do
+        {
+            PrintMainMenu();
+            Scanner input = new Scanner(System.in);
+            String submenu_choice = input.nextLine();
+            if (submenu_choice.length() == 1) // to prevent input of "ENTER" key and to prevent input as words
+                letter = Character.toLowerCase(submenu_choice.charAt(0));
+
+            switch(letter)
+            {
+                case 'a':
+                    System.out.println("Display Submenu A\n");
+                    break;
+                case 'b':
+                    System.out.println("Display Submenu B\n");
+                    break;
+                case 'c':
+                    System.out.println("Display Submenu C\n");
+                    break;
+                case 'd':
+                    System.out.println("Display Connect Four Game\n");
+                    break;
+                case 'e':
+                    System.out.println("You chose to exit the program.\n");
+                    break;
+                default:
+                    System.out.println("\nYou entered an invalid choice. Try again!\n");
+            }
+        } while (!ValidSubmenu(letter));
+
+
+    }
+}
